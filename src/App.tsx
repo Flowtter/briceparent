25fd832d98492b22ae6af497ca51b812e18d22e1
Flowtter <naexys.naexys@gmail.com>
2021-06-09 13:36:39 +0100

    Mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Hobbies from './components/hobbies';
-import Introduction from './components/Introduction';
-import Contact from './components/contact';
-import Feature from './components/feature';
-import Waves from './components/waves';
+// import Introduction from './components/Introduction';
+// import Contact from './components/contact';
+// import Feature from './components/feature';
+// import Waves from './components/waves';
 
 import './styles.css';
 
